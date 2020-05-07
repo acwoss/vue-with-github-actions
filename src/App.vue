814344223840e40cc4f8f3221f0364dd04807eff
Anderson Carlos Woss <anderson.woss@madeiramadeira.com.br>
2020-05-07 11:11:54 -0300

    Alterações

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Está em produção"/>
+    <HelloWorld msg="Está em produção alterado"/>
   </div>
 </template>
 
